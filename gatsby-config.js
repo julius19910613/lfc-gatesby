cfb04990870df09e298400a6990218f8089b374b
julius.li <julius.li@yijinin.com>
2021-06-11 15:26:35 +0800

    add picture plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: "lfc-gatsby",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
